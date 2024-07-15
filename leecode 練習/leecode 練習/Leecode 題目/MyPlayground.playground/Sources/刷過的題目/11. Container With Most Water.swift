f819bca17f0ffc1d834dafc333a54cea78603231
EnolaZhu <enola.zhu@ijoyful.com>
2024-07-15 18:16:20 +0800

    [Two  Points] removeDuplicates

--- leecode 練習/leecode 練習/Leecode 題目/MyPlayground.playground/Sources/刷過的題目/11. Container With Most Water.swift
+++ leecode 練習/leecode 練習/Leecode 題目/MyPlayground.playground/Sources/刷過的題目/11. Container With Most Water.swift
@@ -13,3 +13,20 @@ class Solution11 {
         return maxArea
     }
 }
+
+//func maxArea(_ height: [Int]) -> Int {
+//    var left = 0
+//    var right = height.count - 1
+//    var maxArea = 0
+//    while left < right {
+//        let width = right - left
+//        let area = min(height[left], height[right]) * width
+//        maxArea = max(maxArea, area)
+//        if height[left] < height[right] {
+//            left += 1
+//        } else {
+//            right -= 1
+//        }
+//    }
+//    return maxArea
+//}
---
+++ leecode 練習/leecode 練習/Leecode 題目/MyPlayground.playground/Sources/刷過的題目/Two Points/26 Remove Duplicates from Sorted Array.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+
+// 题目要求你从一个已排序的数组中删除重复项，并返回新的数组的长度。你需要在原地修改输入数组并使得新数组的每个元素只出现一次。
+
+class Solution26 {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+
+    }
+}
