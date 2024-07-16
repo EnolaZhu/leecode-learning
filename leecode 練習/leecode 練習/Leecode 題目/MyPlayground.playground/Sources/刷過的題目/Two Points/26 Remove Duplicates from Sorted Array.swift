bed88bf71c2b2a54e201836efb75363a03f3dac2
EnolaZhu <enola.zhu@ijoyful.com>
2024-07-16 11:48:09 +0800

    [Two  Points] removeDuplicates

--- leecode 練習/leecode 練習/Leecode 題目/MyPlayground.playground/Sources/刷過的題目/Two Points/26 Remove Duplicates from Sorted Array.swift
+++ leecode 練習/leecode 練習/Leecode 題目/MyPlayground.playground/Sources/刷過的題目/Two Points/26 Remove Duplicates from Sorted Array.swift
@@ -5,6 +5,20 @@ import Foundation
 
 class Solution26 {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
+        // inout 傳進來的參數
+        guard nums.count > 1 else {
+            return nums.count
+        }
 
+        var left = 0
+
+        for right in 1..<nums.count {
+            if nums[left] != nums[right] {
+                // 如果 nums[fast] 和 nums[slow] 不同，则说明找到了一个新的不重复的元素，将 slow 前移一位并更新 nums[slow] 为 nums[fast]
+                left += 1
+                nums[left] = nums[right]
+            }
+        }
+        return left + 1
     }
 }
